43ca0ae2bed651c0ed721f258e549693cda91819
DenserMeerkat <mragulmanoharan@gmail.com>
2026-05-28 21:58:12 +0530

    revert: clipboard overriding

--- hyphen/src/androidMain/kotlin/com/denser/hyphen/ui/internal/ClipboardUtils.android.kt
+++ hyphen/src/androidMain/kotlin/com/denser/hyphen/ui/internal/ClipboardUtils.android.kt
@@ -44,19 +44,7 @@ internal actual fun rememberMarkdownClipboard(
                 val start = lastKnownSelection.start.coerceAtMost(lastKnownSelection.end)
                 val end = lastKnownSelection.start.coerceAtLeast(lastKnownSelection.end)
 
-                val clipText = try {
-                    clipEntry.clipData.getItemAt(0)?.text?.toString() ?: ""
-                } catch (e: Exception) {
-                    ""
-                }
-                
-                val selectedText = if (start < end && end <= lastKnownText.length) {
-                    lastKnownText.substring(start, end)
-                } else {
-                    ""
-                }
-
-                if (selectedText.isNotEmpty() && clipText == selectedText) {
+                if (start < end && end <= lastKnownText.length) {
                     val markdown = MarkdownSerializer.serialize(lastKnownText, lastKnownSpans, start, end)
                     val clipData = ClipData.newPlainText(clipboardLabel, markdown)
 
